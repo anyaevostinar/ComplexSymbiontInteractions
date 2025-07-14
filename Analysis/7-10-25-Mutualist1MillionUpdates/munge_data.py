cbd98de0f906d2fd2e4321a2c9eb5d1df1004eb5
EliasH-E <hillmane@carleton.edu>
2025-07-14 10:16:28 -0500

    fixed munge

--- Analysis/7-10-25-Mutualist1MillionUpdates/munge_data.py
+++ Analysis/7-10-25-Mutualist1MillionUpdates/munge_data.py
@@ -18,8 +18,9 @@ for t in treatment_postfixes:
     for r in reps:
         fname = folder +"Tasks_" + t +"_SEED" + str(r)+ ".data"
         uid = t + "_" + str(r)
-        if(os.path.isfile(fname) == false):
+        if(os.path.isfile(fname) == False):
             continue
+
         curFile = open(fname, 'r')
         for line in curFile:
             if (line[0] != "u"):
