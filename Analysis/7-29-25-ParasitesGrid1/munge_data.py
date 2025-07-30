d9411860ee48d70b8a53b338152864058540d971
Taha Khalili <khalilia@carleton.edu>
2025-07-30 09:28:48 -0500

    parasites grid enabled

---
+++ Analysis/7-29-25-ParasitesGrid1/munge_data.py
@@ -0,0 +1,38 @@
+import os.path
+import gzip  
+
+folder = '../../Data/7-29-25-ParasitesGrid1/'
+
+treatment_postfixes = ["MOI0.0", "MOI1.0"]
+partners = ["Host", "Sym"]
+tasks = ["NOT", "NAND", "AND", "ORN", "OR", "ANDN", "NOR", "XOR", "EQU"]
+reps = range(1, 30)
+header = "uid treatment rep update task count partner\n"
+
+outputFileName = "munged_basic.dat"
+
+with open(outputFileName, 'w') as outFile:
+    outFile.write(header)
+
+    for t in treatment_postfixes:
+        for r in reps:
+            fname = folder + "Tasks_" + t + "_SEED" + str(r) + ".data"
+            uid = t + "_" + str(r)
+
+            try:
+                with open(fname, 'r') as curFile:
+                    for line in curFile:
+                        if line[0] != "u":
+                            splitline = line.strip().split(',')
+                            for task_i in range(1, len(splitline), 2):
+                                task = tasks[(task_i - 1) // 2]
+                                host_outstring = "{} {} {} {} {} {} {}\n".format(
+                                    uid, t, r, splitline[0], task, splitline[task_i], "Host")
+                                outFile.write(host_outstring)
+                                sym_outstring = "{} {} {} {} {} {} {}\n".format(
+                                    uid, t, r, splitline[0], task, splitline[task_i + 1], "Parasite")
+                                outFile.write(sym_outstring)
+
+            except FileNotFoundError:
+                print(f"File not found, skipping: {fname}")
+                continue
