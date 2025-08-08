d2bdf35afcac5f56c617af6debfec16c30f934fc
Taha Khalili <khalilia@carleton.edu>
2025-08-08 09:38:30 -0500

    parasites 500k

--- Analysis/8-4-25-Parasites500k/munge_data.py
+++ Analysis/8-4-25-Parasites500k/munge_data.py
@@ -8,9 +8,8 @@ folder = '../../Data/8-4-25-Parasites500k/'
 treatment_postfixes = ["MOI0.0", "MOI1.0"]
 partners = ["Host", "Sym"]
 tasks = ["NOT", "NAND", "AND", "ORN", "OR", "ANDN", "NOR", "XOR", "EQU"]
-reps = range(1,10)
-reps2 = range(10,20)
-reps3 = range(20,31)
+reps = range(1,15)
+reps2 = range(15,31)
 header = "uid treatment rep update task count partner\n"
 
 outputFileName = "munged_basic.dat"
@@ -61,30 +60,6 @@ for t in treatment_postfixes:
                     outFile2.write(sym_outstring)
         curFile.close()
 outFile2.close()
-
-outputFileName = "munged_basic3.dat"
-
-outFile = open(outputFileName, 'w')
-outFile.write(header)
-
-for t in treatment_postfixes:
-    for r in reps3:
-        fname = folder +"Tasks_" + t +"_SEED" + str(r)+ ".data"
-        uid = t + "_" + str(r)
-        
-
-        curFile = open(fname, 'r')
-        for line in curFile:
-            if (line[0] != "u"):
-                splitline = line.strip().split(',')
-                for task_i in range(1, len(splitline), 2):
-                    task = tasks[(task_i-1)//2]
-                    host_outstring = "{} {} {} {} {} {} {}\n".format(uid, t, r, splitline[0], task, splitline[task_i], "Host")
-                    outFile.write(host_outstring)
-                    sym_outstring = "{} {} {} {} {} {} {}\n".format(uid, t, r, splitline[0], task, splitline[task_i+1], "Parasite")
-                    outFile.write(sym_outstring)
-        curFile.close()
-outFile.close()
 
 
 
